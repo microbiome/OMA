b1ff6f516ab8b57039e6982a3e942328cc5c2e95
antagomir <leo.lahti@iki.fi>
2023-04-10 18:05:25 +0300

    polished

--- build.R
+++ build.R
@@ -27,6 +27,6 @@ pkgs <- c(priority, setdiff(pkgs, priority))
 # Store the library list
 write.table(pkgs, file = "oma_packages.csv", row.names=FALSE, col.names=FALSE, quote=FALSE)
 
-# Build the book
-bookdown::render_book("index.Rmd", "bookdown::gitbook")
+# Build the book - must be run separately
+#bookdown::render_book("index.Rmd", "bookdown::gitbook")
 
