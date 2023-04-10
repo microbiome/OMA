5e30b5775cc6fc265d7095b9436de18c4a7bbf64
antagomir <leo.lahti@iki.fi>
2023-04-10 17:10:22 +0300

    urls updated

--- R/95_resources.R
+++ R/95_resources.R
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 
-||||||| merged common ancestors
-=======
-## ----setup, echo=FALSE, results="asis"----------------------------------------
-library(rebook)
-chapterPreamble()
-
->>>>>>> a4695d89c40cde94395c726a99a6073cf631e43a
