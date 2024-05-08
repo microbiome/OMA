8c60608715b823d73f78061079c003cc9aba2820
ake123 <akeketema@yahoo.com>
2024-05-08 17:10:43 +0300

    automated update for oma_packages csv file

---
+++ extract_packages.R
@@ -0,0 +1,25 @@
+packages <- function() {
+  lines <- readLines("DESCRIPTION")
+  
+  extract_packages <- function(lines, section_name) {
+    section_index <- grep(paste0("^", section_name, ":"), lines)
+    if (length(section_index) > 0) {
+      start_index <- section_index + 1
+      end_index <- ifelse(any(grep("^[A-Za-z]+:", lines[-(1:section_index)])),
+                          min(grep("^[A-Za-z]+:", lines[-(1:section_index)])) + section_index - 1,
+                          length(lines))
+      section_lines <- lines[start_index:end_index]
+      section_string <- paste(section_lines, collapse = " ")
+      packages <- unlist(strsplit(section_string, ","))
+      packages <- trimws(packages)
+      return(packages[packages != ""])
+    } else {
+      return(character(0))
+    }
+  }
+  
+  suggests_packages <- extract_packages(lines, "Suggests")
+  write.table(suggests_packages, "oma_packages.csv", row.names = FALSE, col.names = FALSE, sep = ",", quote = FALSE)
+}
+
+packages()
