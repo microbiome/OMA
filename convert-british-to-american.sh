51187d3ee7ef6af2520b9c210205177abc802643
Artur Sannikov <git-sign@asannikov.com>
2026-01-16 10:15:31 +0200

    chore: first test of conversion script

---
+++ convert-british-to-american.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+find . -type f -print0 -iname "*.r" -or -iname "*.[qr]md" -or -iname "*.[r]markdown" |
+    xargs -0 sed -i \
+        -e 's/visualize/visualize/' \
+        -e 's/summarize/summarize/' \
+        -e 's/analyzed/analyzed/' \
+        -e 's/\<analyze\>/analyze/' \
+        -e 's/favor/favour/' \
+        -e 's/visualisation/visualization/'
